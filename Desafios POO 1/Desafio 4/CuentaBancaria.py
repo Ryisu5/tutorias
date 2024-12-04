e5909beb19fbc815925ac53455d57c0f8f8ba055
Ryisu5 <perochinojesus123@gmail.com>
2024-12-04 11:25:27 -0300

    desafio 4 corregido
    falta final

--- Desafios POO 1/Desafio 4/CuentaBancaria.py
+++ Desafios POO 1/Desafio 4/CuentaBancaria.py
@@ -4,12 +4,13 @@ class CuentaBancaria:
 		self.nombre=nombre
 		self.apellido=apellido
 		self.saldo=100000 #saldo de inicio
-		def depositar (self, cantidad):
-			if cantidad > 0:
+	
+	def depositar (self, cantidad):
+		if cantidad > 0:
 			self.saldo+=cantidad
 			print (f"Se han depositado{cantidad}. Nuevo saldo: {self.saldo}")
-			else: 
-				print ("La cantidad depositada debe ser positiva.")
+		else: 
+			print ("La cantidad depositada debe ser positiva.")
 
 	def retirar (self, cantidad):
 		if cantidad > 0:
--- Desafios POO 1/Desafio 4/Main.py
+++ Desafios POO 1/Desafio 4/Main.py
@@ -1,5 +1,5 @@
 from CuentaBancaria import CuentaBancaria
-def main:
+def main():  # definiendo una funcion main
 	numerocuenta=input ("Ingrese el numero de cuenta:")
 	nombre=input("Ingrese su nombre:")
 	apellido=input ("Ingrese su apellido:")
@@ -13,18 +13,21 @@ def main:
 		print("3. Ver saldo")
 		print ("4. Salir")
 
-	opcion=input("Seleccione una opcion (1-4):")
+		opcion=input("Seleccione una opcion (1-4):")
 
-	if opcion=="1":
-		cantidad=int(input("Ingrese la cantidad a depositar:"))
-		cuenta.depositar(cantidad)
-	elif opcion=="2"
-		cantidad=int(input("Ingrese a cantidad a retirar:"))
-		cuenta.retirar(cantidad)
-	elif opcion=="3"
-		cuenta.versaldo()
-	elif opcion=="4"
-		print ("Saliendo...")
-		break
-	else:
-		print("Opcion no valida.Intente de nuevo")
+		if opcion=="1":
+			cantidad=int(input("Ingrese la cantidad a depositar:"))
+			cuenta.depositar(cantidad)
+		elif opcion=="2":
+			cantidad=int(input("Ingrese a cantidad a retirar:"))
+			cuenta.retirar(cantidad)
+		elif opcion=="3":
+			cuenta.versaldo()
+		elif opcion=="4":
+			print ("Saliendo...")
+			break
+		else:
+			print("Opcion no valida.Intente de nuevo")
+
+if __name__ == "__main__":
+	main()
