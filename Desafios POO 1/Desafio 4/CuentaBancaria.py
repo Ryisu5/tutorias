7cdf73dffe1a228ebf7f1e1ab075fa5832a3b5bd
Agustina Guadalupe Muñoz Arruti <guada20munozarruti@gmail.com>
2024-12-04 11:16:09 -0300

    Desafio 4

--- Desafios POO 1/Desafio 4/CuentaBancaria.py
+++ Desafios POO 1/Desafio 4/CuentaBancaria.py
@@ -1,1 +1,29 @@
-Class CuentaBancaria:
+class CuentaBancaria:
+	def __init__(self, numerocuenta, nombre, apellido):
+		self.numerocuenta=numerocuenta
+		self.nombre=nombre
+		self.apellido=apellido
+		self.saldo=100000 #saldo de inicio
+		def depositar (self, cantidad):
+			if cantidad > 0:
+			self.saldo+=cantidad
+			print (f"Se han depositado{cantidad}. Nuevo saldo: {self.saldo}")
+			else: 
+				print ("La cantidad depositada debe ser positiva.")
+
+	def retirar (self, cantidad):
+		if cantidad > 0:
+			if cantidad <= self.saldo:
+				self.saldo-=cantidad
+				print (f"Se han retirando {cantidad}.Nuevo Saldo: {self.saldo}.")
+			else: 
+				print ("Fondos insuficientes para realizar el retiro.")
+		else: 
+			print ("La cantidad a retirar debe ser positiva")
+
+	def versaldo (self):
+		print (f"Saldo actual: {self.saldo}")
+		
+
+
+
